ceabbeacc80417882f11f9eba9af0037714ad6c8
malik31200 <yka-vtc@outlook.fr>
2026-08-04 14:07:48 +0200

    add post user in controllers

---
+++ controllers/UsersController.js
@@ -0,0 +1,50 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({
+        error: 'Missing email',
+      });
+    }
+
+    if (!password) {
+      return res.status(400).json({
+        error: 'Missing password',
+      });
+    }
+
+    const user = await dbClient
+      .client
+      .db()
+      .collection('users')
+      .findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        error: 'Already exist',
+      });
+    }
+
+    const hashedPassword = sha1(password);
+
+    const result = await dbClient
+      .client
+      .db()
+      .collection('users')
+      .insertOne({
+        email,
+        password: hashedPassword,
+      });
+
+    return res.status(201).json({
+      id: result.insertedId,
+      email,
+    });
+  }
+}
+
+export default UsersController;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use('/', router);
 
 app.listen(port, () => {
